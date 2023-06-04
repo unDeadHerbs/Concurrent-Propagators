136e3d6c43e19ccd4612c52d42b272d82759e219
Murray Fordyce <unDeadHerbs@gmail.com>
2023-06-04 18:09:37 -0500

    very basic propogators

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+struct Propogator{virtual void alert()=0;};
+struct Cell{
+	std::vector<Propogator*> watchers;
+	void updated(){for(auto w:watchers)w->alert();}
+};
+
+struct StringCell:Cell{
+	std::string value;
+	void set(std::string v){value=v;updated();}
+};
+struct AutoPrinter:Propogator{
+	StringCell* watched;
+	AutoPrinter(StringCell*w):watched(w){watched->watchers.push_back(this);}
+	void alert(){std::cout<<watched->value<<std::flush;}
+};
 
 int main(){
-	std::cout<<"hello world"<<std::endl;
+	StringCell sc;
+	AutoPrinter ap(&sc);
+	sc.set("hello world\n");
 }
